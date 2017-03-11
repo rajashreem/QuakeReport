e3ec4c9ae802793c66df94f01ac9c1894da33ad3
Rajashree Malvade <rajashreermalvade@gmail.com>
2017-03-11 15:22:36 +0530

    Update the preference summary whenever the value is changed

--- app/src/main/java/com/example/android/quakereport/SettingsFragment.java
+++ app/src/main/java/com/example/android/quakereport/SettingsFragment.java
@@ -1,13 +1,48 @@
 package com.example.android.quakereport;
 
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.Preference;
 import android.preference.PreferenceFragment;
 
-public class SettingsFragment extends PreferenceFragment {
+public class SettingsFragment extends PreferenceFragment implements SharedPreferences.OnSharedPreferenceChangeListener {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         addPreferencesFromResource(R.xml.preferences);
+
+        bindSummaryToPreferences();
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        if(key.equals(getString(R.string.settings_min_magnitude_key))){
+            Preference preference = findPreference(key);
+            preference.setSummary(sharedPreferences.getString(key, ""));
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
+        sharedPreferences.registerOnSharedPreferenceChangeListener(this);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
+        sharedPreferences.unregisterOnSharedPreferenceChangeListener(this);
+    }
+
+    private void bindSummaryToPreferences() {
+        SharedPreferences sharedPreferences = getPreferenceManager().getSharedPreferences();
+
+        Preference minMagnitudePreference = findPreference(getString(R.string.settings_min_magnitude_key));
+        minMagnitudePreference.setSummary(sharedPreferences.getString(minMagnitudePreference.getKey(), ""));
     }
 }
